0115f4e4746292ef1f091292fca5c420d37c03f4
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-05-10 14:11:41 +0500

    about form updated

--- Phonebook Application/Phonebook Application/Properties/Resources.Designer.cs
+++ Phonebook Application/Phonebook Application/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace Phonebook_Application.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap pk {
+            get {
+                object obj = ResourceManager.GetObject("pk", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap recordicon {
             get {
                 object obj = ResourceManager.GetObject("recordicon", resourceCulture);
